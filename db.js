251da511d8428d583904922148677bf7be250cde
alevscyg <110639375+alevscyg@users.noreply.github.com>
2024-05-29 09:40:31 +0500

    Update db.js

--- db.js
+++ db.js
@@ -5,27 +5,41 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(255) NOT NULL,
     age TINYINT NOT NULL
 )`);
+
 module.exports = {
     createUser: async(name, age) => {
-        db.run(`INSERT INTO users (name, age) VALUES (?, ?)`, [name, age]);
-        return 'THE USER HAS BEEN CREATED';
+        return await new Promise((resolve, reject) => { db.run(`INSERT INTO users (name, age) VALUES (?, ?)`, [name, age], (err) => { 
+                if(err) reject(err);
+                else resolve('THE USER HAS BEEN CREATED');
+            } 
+        )});
     },
     getUsers: async() => {
-        return await new Promise((resolve) => {
-            db.all(`SELECT * FROM users`, (err, row) => {resolve(row)});
-        });
+        return await new Promise((resolve, reject) => { db.all(`SELECT * FROM users`, (err, row) => {
+                if(row) resolve(row);
+                else reject(err);
+            } 
+        )});
     },
     getUserById: async(id) => {
-        return await new Promise((resolve) => {
-            db.get(`SELECT * FROM users WHERE id = ?`, [id], (err, row) => {resolve(row)});
-        });
+        return await new Promise((resolve, reject) => { db.get(`SELECT * FROM users WHERE id = ?`, [id], (err, row) => {
+                if(row) resolve(row);
+                else reject(err);
+            }
+        )});
     },
     deleteUser: async(id) => {
-        return db.run(`DELETE FROM users WHERE id = ?`, [id]);
+        return await new Promise((resolve, reject) => { db.run(`DELETE FROM users WHERE id = ?`, [id] , (err) => { 
+                if(err) reject(err);
+                else resolve(`THE USER WITH ID = ${id} HAS BEEN DELETED`);
+            } 
+        )});
     },
     updateUser: async(name, age, id) => {
-        db.run(`UPDATE users SET name = ?, age = ? WHERE id = ?`,[name, age, id]);
-        return `THE USER WITH ID = ${id} HAS BEEN UPDATED`;
+        return await new Promise((resolve, reject) => { db.run(`UPDATE users SET name = ?, age = ? WHERE id = ?`, [name, age, id], (err) => { 
+            if(err) reject(err);
+            else resolve(`THE USER WITH ID = ${id} HAS BEEN UPDATED`);
+            } 
+        )});
     }
-
 };
